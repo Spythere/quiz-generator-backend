558795272f4e0a342a12c3bc5850309489e02410
Spythere <spythere@protonmail.com>
2022-12-13 14:22:25 +0100

    Auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { QuestionsModule } from './questions/questions.module';
 import { QuizesModule } from './quizes/quizes.module';
 import { SectionsModule } from './sections/sections.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [QuestionsModule, QuizesModule, SectionsModule],
+  imports: [QuestionsModule, QuizesModule, SectionsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
